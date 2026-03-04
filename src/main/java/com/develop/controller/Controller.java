e68c9b73f9f210a7241f7b20a35603403986654b
Ulises Uriel <ucantor@tecnodalpha.com>
2026-03-03 22:21:27 -0600

    Initial Spring Boot project

---
+++ src/main/java/com/develop/controller/Controller.java
@@ -0,0 +1,15 @@
+package com.develop.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	
+	@GetMapping("/ping")
+	public String ping() {
+	        return "hello word!!!";
+	}
+	    
+
+}
